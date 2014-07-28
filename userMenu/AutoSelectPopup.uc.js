69c6ea8ff2964671483a4739b75c2f52846b1aff
Drager-oos <Drager-oos@users.noreply.github.com>
2014-07-28 22:54:06 +0800

    Update AutoSelectPopup.uc.js

--- userMenu/AutoSelectPopup.uc.js
+++ userMenu/AutoSelectPopup.uc.js
@@ -1,19 +1,19 @@
-(function() {
+location == "chrome://browser/content/browser.xul" && (function() {
 	var ASP = {
 		init: function() {
 			var popup = $("mainPopupSet").appendChild($C("menupopup", {
 				id: "auto-popup",
 				style: "-moz-appearance: none; background: -moz-linear-gradient(top, rgb(252, 252, 252) 0%, rgb(245, 245, 245) 33%, rgb(245, 245, 245) 100%); border: 2px solid rgb(144,144,144); border-radius: 5px;"
 			}));
-			var menugroup = popup.appendChild($C("menugroup"));
+			var menugroup = popup.appendChild($C("menugroup", {onclick: "document.getElementById('auto-popup').hidePopup();"}));
 
 			for (let i = 0, menu; menu = mMenus[i]; i++) {
-				let menuItem = menugroup.appendChild($C("menuitem", {
+				let menuItem = menugroup.appendChild($C("toolbarbutton", {
 					tooltiptext: menu.label,
 					image: menu.image,
 					class: "menuitem-iconic",
 					onclick: menu.onclick,
-					style: menu.style || "max-width: 10px;"
+					style: menu.style
 				}));
 			}
 		}
@@ -22,7 +22,10 @@
 		{
 			label: '左鍵：複製為純文字\n右鍵：複製',
 			image: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAABlSURBVDhP5Y5BCsAgEAP3i/1AP+D/zxUlwWBXXQueOhAQzQStcN3p2UmVFK80C7QGH1aEBniOBPqhgRnsQB8P8KzRe+i/+YHCO+htQNPjdaB/G4D6hoWekFzQohfUxngSg4pglgGUsQ0ZR4jGSwAAAABJRU5ErkJggg==",
-			onclick: "if (event.button == 0) {Components.classes['@mozilla.org/widget/clipboardhelper;1'].getService(Components.interfaces.nsIClipboardHelper).copyString(content.getSelection());} else if (event.button == 2) {goDoCommand('cmd_copy'); document.getElementById('auto-popup').hidePopup();}"
+			onclick: "\
+			if (event.button == 0) {Components.classes['@mozilla.org/widget/clipboardhelper;1'].getService(Components.interfaces.nsIClipboardHelper).copyString(content.getSelection());}\
+			else if (event.button == 2) {goDoCommand('cmd_copy'); document.getElementById('auto-popup').hidePopup();}\
+			"
 		},
 		{
 			label: "高亮",
@@ -33,17 +36,20 @@
 			label: "左鍵：Google 加密\n中鍵：Google 加密站內\n右鍵：Google 加密圖片",
 			image: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAABWUlEQVQ4jaXTPUvDQBgH8HyzkiCVdlBcFD+CDgUn0bU5rUMRS6mD4BuCVgfFKmitCl0s+FKhvoEgVvsyWKuRS9JLcvm7tcplSHW44e6e5/c8x91JAaKFZJXWFELRzZBVWgsQLST9JfknInlt9ExRJLMMqSOG67ID7gLb5xbG100h1hNIFyzM51gbu61wnN7Znl14Al+GC7LTas9nMi20bPgHPnUXmatOxbE1E89v3D8wd8DAbGBiw0R/XMfupY3RJcM/oBCKkUUDiUMGF/h1HN+AQiiC0xSa4aL04mBgVvcPTKZNbBYspHIMy3mGJnXx+s4xmBARAVg4Ybh4ctAb66wNJXSUGxx7RfEqBaDa5EgdMSEwmWXIlnwA+Qcb5QbHcLLTbjBGcfboILLq4yX2xXVsFSzUP1zcVzmOb2zsF21EVsRkhVD89zPVJTmqhWWV1rsGVFqRo1r4G6iM33AbQTj+AAAAAElFTkSuQmCC",
 			onclick: "\
-			if (event.button == 0) {gBrowser.selectedTab = gBrowser.addTab('https://encrypted.google.com/#q=' + encodeURIComponent(getBrowserSelection()));}\
-			else if (event.button == 1) {gBrowser.selectedTab = gBrowser.addTab('https://encrypted.google.com/#q=' + content.location.host + ' ' + encodeURIComponent(getBrowserSelection())); document.getElementById('auto-popup').hidePopup();}\
-			else if (event.button == 2) {gBrowser.selectedTab = gBrowser.addTab('https://duckduckgo.com/?q=!img ' + encodeURIComponent(getBrowserSelection())); document.getElementById('auto-popup').hidePopup();}\
+			var TXT = encodeURIComponent(getBrowserSelection());\
+			if (event.button == 0) {gBrowser.selectedTab = gBrowser.addTab('https://encrypted.google.com/#q=' + TXT);}\
+			else if (event.button == 1) {gBrowser.selectedTab = gBrowser.addTab('https://encrypted.google.com/#q=' + content.location.host + ' ' + TXT);}\
+			else if (event.button == 2) {gBrowser.selectedTab = gBrowser.addTab('https://duckduckgo.com/?q=!img ' + TXT);}\
 			",
 		},
 		{
 			label: "左鍵：百度\n中鍵：百度貼吧\n右鍵：百度圖片",
 			image: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAABWUlEQVQ4jaWSy0sCURjF/XfuRugukha1CzeBCBKIFFFIBEGrCoRwE4EErlskoYW0EFy0iBAkCMFNBCGuKrqjNg6OgzOTjY+5nhbh3ehMrw/O8vud73E8hDL8Rx5CGf5ajoBCsQuvT0IubwIATk51xA/bsPkPAdFtBYQyLIXeUCpbYtybQtcd0Na+LHb2WiCUYTXaRC5vCsBdyXIG3D/0QCjD2qaCl9cB9g9UPFb66OgcuzEVmayBpmKjVLamAxJJTTg9PQ+mHm1+sQ5CGS4ujUlAJmuAUIaZOQkdnaNS7SMYlhGKyKjVh7B6I2EQi6uTAJsDV9fvqFT7YNIQsws10eAPNNDWODa2FHh9Eoq3H85faKk2/IHGRGCWV2RYvZH7Fzo6n9o8VmS9CcPkzoBUWv82umfnhjNgfEg3pdK6M8AwuUihP9DA0bGGRFJDMCyLYLmu8NsSgP/oExgMERjFwInkAAAAAElFTkSuQmCC",
-			onclick: "if (event.button == 0) {gBrowser.selectedTab = gBrowser.addTab('http://www.baidu.com/s?ie=utf-8&wd=' + encodeURIComponent(getBrowserSelection()));}\
-			else if (event.button == 1) {gBrowser.selectedTab = gBrowser.addTab('https://duckduckgo.com/?q=!tieba ' + encodeURIComponent(getBrowserSelection())); document.getElementById('auto-popup').hidePopup();}\
-			else if (event.button == 2) {gBrowser.selectedTab = gBrowser.addTab('http://image.baidu.com/i?&cl=2&ie=utf-8&oe=utf-8&word=' + encodeURIComponent(getBrowserSelection())); document.getElementById('auto-popup').hidePopup();}\
+			onclick: "\
+			if (event.button == 0) {var url = 'http://www.baidu.com/s?ie=utf-8&wd=';}\
+			else if (event.button == 1) {var url = 'https://duckduckgo.com/?q=!tieba ';}\
+			else if (event.button == 2) {var url = 'http://image.baidu.com/i?&cl=2&ie=utf-8&oe=utf-8&word=';}\
+			gBrowser.selectedTab = gBrowser.addTab(url + encodeURIComponent(getBrowserSelection()));\
 			",
 		},
 	];
@@ -54,11 +60,12 @@
 		if (attr) Object.keys(attr).forEach(function(n) el.setAttribute(n, attr[n]));
 		return el;
 	}
-	location == "chrome://browser/content/browser.xul" && gBrowser.mPanelContainer.addEventListener("mouseup", function (event) {
+	gBrowser.mPanelContainer.addEventListener("mouseup", function (event) {
 		var eName = event.target.nodeName || event.target.localName || event.target.tagName;
 		if (eName == "TEXTAREA" || eName == "INPUT" || event.target.isContentEditable) return;
+		if (!getBrowserSelection()) return;
 		if (event.button == 0 && getBrowserSelection()) {
-			document.getElementById("auto-popup").openPopup(null, null, event.screenX - 100, event.screenY + 20);
+			$("auto-popup").openPopup(null, null, event.screenX - 100, event.screenY + 20);
 		}
 	}, false);
 })();
