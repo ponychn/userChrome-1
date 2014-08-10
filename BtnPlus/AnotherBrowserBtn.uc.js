bd8371bd115f14ab0a4bf1eb4aaa5740361a8ede
Drager-oos <Drager-oos@users.noreply.github.com>
2014-08-10 16:16:50 +0800

    Update AnotherBrowserBtn.uc.js

--- BtnPlus/AnotherBrowserBtn.uc.js
+++ BtnPlus/AnotherBrowserBtn.uc.js
@@ -1,14 +1,14 @@
 (function() {
-	window.AnotherBrowser = {
+	AnotherBrowser = {
 		init: function() {
-			var bar = "TabsToolbar";
-			this.icon = $(bar).appendChild($C("toolbarbutton", {
+			this.icon = $("TabsToolbar").appendChild($C("toolbarbutton", {
 				id: "AnotherBrowser",
 				class: "toolbarbutton-1",
 				label: "另一個視窗",
 				tooltiptext: "左鍵：顯示 Index\n中鍵：顯示收藏庫\n右鍵：顯示 Website",
 				image: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAArUlEQVQ4jc2TUQrDIBBE3wVMcoEk90iOmx5C/wO1f80FhNqL9CMDXYpCIIVWGFzX2dHVXYA7kIFk8AQ80ApePsvJiuUBTEAPjJpn4Cq7lz1/cCbFkuS0YwRWI7BWOOk/BLLyG+QctI5GIFY4GSCIsBrcgAvghEU+y4mKpTMnWTTmuk2F08H+z6VNZwRchdPCXiSlFJYDKfivPOLv6+C0wOlm2ii3c+DdzoFyO28vxlBcNJTkO0QAAAAASUVORK5CYII=",
 				onclick: "AnotherBrowser.openPanel(event);",
+				style: "padding: 0px; -moz-transform: scale(0.875);",
 			}));
 
 			var panel = $("mainPopupSet").appendChild($C("panel", {
@@ -32,28 +32,20 @@
 			}));
 		},
 		openPanel: function(event) {
-			var self = this;
-			var panel = $("AnotherBrowser-panel"),
+			var self = this,
+				panel = $("AnotherBrowser-panel"),
 				iframe = $("AnotherBrowser-iframe");
-			var openPopup = function() {
-				panel.openPopup(self.icon, "after_end", -8, 0, false, null, null);
-			};
-			openPopup();
-			switch(event.button) {
-				case 0:
-					iframe.contentDocument.location.href = "chrome://userchromejs/content/index.html";
-				break;
-				case 1:
-					iframe.contentDocument.location.href = "chrome://browser/content/places/places.xul";
-				break;
-				case 2:
-					iframe.contentDocument.location.href = "chrome://userchromejs/content/Website.html";
-					event.preventDefault();
-				break;
-			}
+			panel.openPopup(self.icon);
+			var url = [
+				"chrome://userchromejs/content/index.html",
+				"chrome://browser/content/places/places.xul",
+				"chrome://userchromejs/content/Website.html"
+			];
+			iframe.contentDocument.location.href = url[event.button];
+			event.preventDefault();
 		},
 	};
-	window.AnotherBrowser.init();
+	AnotherBrowser.init();
 
 	function $(id, doc) (doc || document).getElementById(id);
 	function $C(name, attr) {
