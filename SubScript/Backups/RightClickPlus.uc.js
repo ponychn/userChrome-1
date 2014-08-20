7ad45dcb39e4b2a39818bdd80c8c0b37bb69e2a5
Drager-oos <Drager-oos@users.noreply.github.com>
2014-08-20 08:18:32 +0800

    Update RightClickPlus.uc.js

--- SubScript/Backups/RightClickPlus.uc.js
+++ SubScript/Backups/RightClickPlus.uc.js
@@ -1,23 +1,20 @@
 // ==UserScript==
 // @name			RightClickPlus.uc.js
-// @description		按住右鍵新分頁打開鏈結，在鏈結上 Ctrl + 右鍵能打開選單，雙擊右鍵關閉分頁
+// @description		右鍵新分頁打開鏈結，在鏈結上 Ctrl + 右鍵打開選單，雙擊右鍵關閉分頁
 // @homepageURL		https://github.com/Drager-oos/userChrome/blob/master/SubScript/Backups/RightClickPlus.uc.js
 // ==/UserScript==
 (function() {
-	var Background = true;  // false: 前景 | true: 背景
-	function $(id) document.getElementById(id);
+	var x = true;  // false: 前景 | true: 背景
 
+	function $(id) document.getElementById(id);
+	function isLink(node) {
+		if ((node instanceof HTMLAnchorElement || node instanceof HTMLAreaElement) && node.hasAttribute('href')) return node;
+		return false;
+	}
 	function findLink(element) {
-		// Super_start
-		if (element.className == 'site-snapshot') {
-			return element.parentNode;
-		}
 		switch (element.tagName) {
 			case 'A': return element;
-			case 'B': case 'I': case 'SPAN': case 'SMALL':
-			case 'STRONG': case 'EM': case 'BIG': case 'SUB':
-			case 'SUP': case 'IMG': case 'S':
-			case 'FONT':
+			case 'FONT': case 'U':
 				var parent = element.parentNode;
 				return parent && findLink(parent);
 			default:
@@ -27,21 +24,13 @@
 
 	gBrowser.mPanelContainer.addEventListener('click', function(e) {
 		if (e.button == 2 && !e.ctrlKey) {
-			var link = findLink(e.target);
-			if (link) {
-				var href = link.href;
-				if (href && href.match(/^(https?|ftp|chrome):\/\/|^about:/)) {
-					e.preventDefault();
-					e.stopPropagation();
-					$("contentAreaContextMenu").hidePopup();
-					if (Background == true) {
-						gBrowser.addTab(href);
-					}
-					else if (Background == false) {
-						gBrowser.selectedTab = gBrowser.addTab(href);
-					}
-					return;
-				}
+			var link = isLink(e.target) || isLink(e.target.parentNode) || findLink(e.target),
+				href = link.href;
+			if (href) {
+				e.preventDefault();
+				e.stopPropagation();
+				$("contentAreaContextMenu").hidePopup();
+				gBrowser.loadOneTab(href, {inBackground: x});
 			}
 		}
 	}, false);
